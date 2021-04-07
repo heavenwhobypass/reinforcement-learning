cb648a79e855a4faff36135112d9225680a8c513
ttw <907993189@qq.com>
2021-04-07 15:11:36 +0800

    create QL file

---
+++ Qlearning/ql.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#-*- coding:utf-8 -*-
+############################
+# File Name: ql.py
+# Author: taowang
+# mail: 907993189@qq.com
+# Create Time: 2021/4/7 15:10:43
+############################
+import numpy as np
+import pandas
